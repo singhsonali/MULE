8201288fba9fd3eac6995a17b285245d8bba2da7
Shannor Trotty <shannor.a.trotty@gmail.com>
2015-09-20 18:41:10 -0400

    Was chaning land but stopped

---
+++ src/Model/Land.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created by Shannor on 9/20/2015.
+ * Land Class which will hold mountains, rivers, or nothing
+ * Will provide an certain amount of Resources based on things it owns
+ *
+ */
+public class Land {
+    
+}
