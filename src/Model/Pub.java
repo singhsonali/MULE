3c1f804a0b4b883cea74d73591a3a5ebdac34a2d
Sonali Singh <ssingh@lawn-128-61-44-6.lawn.gatech.edu>
2015-10-01 19:28:38 -0400

    implemented gamble in pub

--- src/Model/Pub.java
+++ src/Model/Pub.java
@@ -1,10 +1,40 @@
 package Model;
-
+import java.util.Random;
 /**
  * Created by Ashley on 9/20/2015.
  */
 public class Pub {
+    
+    private static Pub instance = null;
+    private Pub(){} // Singleton -> private constructor
+    
+    public static Pub getInstance(){
+        if(instance == null){
+            instance = new Pub();
+        }
+        
+        return instance;
+    }
 
-    public Pub() {}
+    /**
+     * calcGamble method to calculate the amount a player can gamble in terms of time
+     * 
+     * @param round
+     * @param timeLeft
+     * @return time lost from player's turn
+     */
+    public static int calcGamble(Round round, long timeLeft){
+        Random rand = new Random();
+        double secondsLeft = (double) timeLeft/1000;
+        
+        if (secondsLeft < 0){
+            secondsLeft = 0;
+        }
+        
+        //System.out.println("Seconds left = " + secondsLeft);
+        return (int)(round.getGamblingBonus() + rand.nextInt((int)(2 * (2.14 * secondsLeft)+1))); // convert seconds to BTU then double
+    }
+
+}
 
 }
