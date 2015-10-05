2a27472e9b32cda4f0a1f7de8d3d3e82bcde37d0
anoll3 <Ashleynoll88@gmail.com>
2015-10-05 17:49:43 -0400

    Set storeController buttons and text fields
    Set storeController buttons and text fields

--- src/view/storeController.java
+++ src/view/storeController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
+import java.awt.*;
+
 /**
  * Created by Ashley on 9/25/2015.
  */
@@ -15,9 +17,14 @@ public class storeController {
 
     private Pane currentPane;
 
+    @FXML
     private Button btnReturn;
+    @FXML
     private Button btnBuy;
+    @FXML
     private Button btnSell;
+    @FXML
+    private TextField amnt;
 
     public storeController() {
 
