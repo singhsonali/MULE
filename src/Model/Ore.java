07a51003e2d31411982919408b1eb71640b843eb
Shannor Trotty <shannor.a.trotty@gmail.com>
2015-10-31 13:44:58 -0400

    Ran all checks on GameTimer and passed within reseaon.

--- src/Model/Ore.java
+++ src/Model/Ore.java
@@ -2,22 +2,37 @@ package model;
 
 /**
  * Created by rileyauten on 9/30/15.
+ * Land Resource class that extends from the Abstract Class AbstractResources.
  */
 public class Ore extends AbstractResources {
 
+    /**
+     * Variable to hold the amount of Ore Owned.
+     */
     private int amount;
 
-    public Ore(){
+    /**
+     * Constructor for Ore.
+     * Init to the default amount.
+     */
+    public Ore() {
+        super();
         this.amount = 0; //default
     }
 
-    @Override
-    void setAmount(int i) {
-        this.amount = i;
+    /**
+     * Sets the amount to a new variable.
+     * @param newAmount The new amount desired for Ore.
+     */
+    final void setAmount(final int newAmount) {
+        this.amount = newAmount;
     }
 
-    @Override
-    int getAmount() {
+    /**
+     * Returns the current value stored in amount.
+     * @return amount
+     */
+    final int getAmount() {
         return this.amount;
     }
 }
