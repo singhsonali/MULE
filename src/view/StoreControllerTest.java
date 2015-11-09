9322e2d4290130438eb536c5dc19ed914c95843c
anoll3 <Ashleynoll88@gmail.com>
2015-11-09 15:10:29 -0500

    Minor Changes
    Minor changes to JUnit

--- src/view/StoreControllerTest.java
+++ src/view/StoreControllerTest.java
@@ -11,37 +11,36 @@ import org.junit.Test;
 public class StoreControllerTest extends TestCase {
 
     private Player player;
-    private StoreController storeController;
-    private static int foodPrice = 30;
+    private StoreController StoreController = new StoreController();
+    //private static int foodPrice = 30;
     private int origFood;
 
     @Before
     public void setUp() {
         player = new Player();
-        storeController = new StoreController();
-        storeController.setCurrentPlayer(player);
+        StoreController.setCurrentPlayer(player);
         origFood = player.getFood();
-        storeController.setStore();
+        StoreController.setStore();
     }
 
     @Test
     public void testBuyFood() {
         //Has money to buy food.
         player.setMoney(30);
-        storeController.buyFood(1);
+        StoreController.buyFood(1);
         assertEquals(player.getMoney(), 0);
         assertEquals(player.getFood(), origFood + 1);
     }
 
-    @Test(expected = IndexOutOfBoundsException.class)
+    @Test (expected = IndexOutOfBoundsException.class)
     public void buyFoodException() {
         player.setMoney(29);
-        storeController.buyFood(1);
+        StoreController.buyFood(1);
     }
 
     @Test (expected = IndexOutOfBoundsException.class)
     public void buyOverInventory() {
         player.setMoney(Integer.MAX_VALUE);
-        storeController.buyFood(17);
+        StoreController.buyFood(17);
     }
 }
